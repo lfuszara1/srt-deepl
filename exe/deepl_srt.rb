2a9fa77f0e8749c2a7c5068b64041b14fbb71c42
Łukasz Fuszara <lukasz@fuszara.pl>
2021-05-27 14:01:04 +0200

    exe

---
+++ exe/deepl_srt.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'deepl_srt'
+
+DeeplSrt.deepl_srt(ARGV[0], ARGV[1], ARGV[2])
--- lib/deepl_srt.rb
+++ lib/deepl_srt.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
 require_relative 'version'
+require_relative './deepl_srt/srt_translate'
 
+# Main module of DeeplSrt
 module DeeplSrt
   class Error < StandardError; end
-  # Your code goes here...
+
+  def deepl_srt(path, target_lang, result_path)
+    srt_translate = SrtTranslate.new(path, target_lang)
+    result = srt_translate.parse
+    File.open(result_path, 'w') { |file| file.write(result) }
+  end
 end
