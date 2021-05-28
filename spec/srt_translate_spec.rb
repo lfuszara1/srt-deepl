e1bd95441c61d6859c3cf4d3be99bc9277d5e827
Łukasz Fuszara <lukasz@fuszara.pl>
2021-05-28 14:50:19 +0200

    Small fix

--- spec/srt_translate_spec.rb
+++ spec/srt_translate_spec.rb
@@ -9,7 +9,7 @@ describe SrtTranslate do
     it 'translate' do
       env = Dotenv.load('.env')
       api_key = env['API_KEY']
-      new_instance = SrtTranslate.new(api_key, 'PL', './examples/example.input.srt', './examples/example.output.tmp')
+      new_instance = SrtTranslate.new(api_key, 'PL', './examples/example.input.srt', './examples/example.output.srt')
       expect(new_instance.parse(0)).to be_instance_of String
     end
   end
